50dd1f62213de699cddc5ec548e7130af26488cb
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-10-02 16:12:38 +0200

    idk man this is tough

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,20 +6,18 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 12
-    number_of_parts = 3
     assert all(
-        element == (value // number_of_parts)
-        for element in split_integer(value, number_of_parts)
+        element == (12 // 3)
+        for element in split_integer(12, 3)
     )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(12, 1) == [12]
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert sorted(split_integer(17, 4)) == split_integer(17, 4)
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
